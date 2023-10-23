5d8533b3a1774950ebccf3e539deb27e434fe9d3
Marus <olegmar11@ukr.net>
2023-10-23 15:07:03 +0300

    adding auth

---
+++ Controllers/UserController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_API;
+using ASP_API.Models;
+
+namespace ASP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly HospitalDBContext _context;
+
+        public UsersController(HospitalDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(int id)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var Users = await _context.Users.FindAsync(id);
+
+            if (Users == null)
+            {
+                return NotFound();
+            }
+
+            return Users;
+        }
+
+        // PUT: api/Userss/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsers(int id, Users Users)
+        {
+            if (id != Users.Uid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(Users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Userss
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostUsers(Users Users)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'HospitalDBContext.Users'  is null.");
+            }
+            _context.Users.Add(Users);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsers", new { id = Users.Uid }, Users);
+        }
+
+        // DELETE: api/Userss/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsers(int id)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var Users = await _context.Users.FindAsync(id);
+            if (Users == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(Users);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsersExists(int id)
+        {
+            return (_context.Users?.Any(e => e.Uid == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,99 @@
+﻿using ASP_API.Attributes;
+using ASP_API.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+using SQLitePCL;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using ASP_API;
+using ASP_API.Controllers;
+
+namespace ASP_API.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, HospitalDBContext context) : base(options, logger, encoder, clock)
+        {
+
+            _context = context;
+            users = context.Users.ToList();
+        }
+
+        public HospitalDBContext _context;
+        public List<Users> users;
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if(!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(
+                    AuthenticateResult.Fail("Missing authorization key"
+                    ));
+            }
+
+            var authorizationHeader = Request.Headers["Authorization"].ToString();
+
+            if (!authorizationHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.FromResult(
+                    AuthenticateResult.Fail("Autnorization header does not contain 'Basic' "
+                    ));
+            }
+
+            var authBase64Decoded = Encoding.UTF8.GetString(Convert.FromBase64String(
+                authorizationHeader.Replace("Basic ","",StringComparison.OrdinalIgnoreCase
+                )));
+            var authSplit = authBase64Decoded.Split(new[] { ':' }, 2);
+
+            if(authSplit.Length != 2) 
+            {
+                return Task.FromResult(
+                   AuthenticateResult.Fail("AuthSplit is not 2"
+                   ));
+            }
+
+            var clientId = authSplit[0];
+            var clientSecret = authSplit[1];
+
+
+            bool valid = false;
+            foreach(var user in users)
+            {
+                if(clientId == user.UserName && clientSecret == user.Password)
+                {
+                    valid = true;
+                }
+            }
+
+            if(!valid)
+            {
+                return Task.FromResult(
+                    AuthenticateResult.Fail("Autnorization failed. ClientId or ClientSecret is not 'test'  "
+                    ));
+            }
+
+            var client = new ClientAuthenticationDefaults
+            {
+                AuthenticationType = "Basic",
+                IsAuthenticated = true,
+                Name = clientId
+            };
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(client, new[]
+            {
+                new Claim(ClaimTypes.Name, clientId)
+            }));
+
+            return Task.FromResult(
+                AuthenticateResult.Success(
+                    new AuthenticationTicket(claimsPrincipal, Scheme.Name
+                    )
+                )
+            );
+        }
+    }
+}
